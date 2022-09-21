d09e02ba336b3d01321d164949c8b5c3362d8139
capocolo <gabrielserna.hce@gmail.com>
2022-09-21 17:51:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import ExpenseItem from './components/ExpenseItem.js';
 
 function App() {
   return (
     <div className="App">
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
