5b9f79a3bcbc7529446a9ce303500b3b7a14a699
capocolo <gabrielserna.hce@gmail.com>
2022-09-23 15:16:58 +0200

    components

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -2,16 +2,16 @@ import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
   const year = props.date.getFullYear();
-  const mount = props.date.toLocaleString('it-IT', { mount: 'long' });
+  const mount = props.date.toLocaleString('it-IT', { month: 'long' });
   const day = props.date.toLocaleString('it-IT', { day: '2-digit' });
 
   return (
-    <div className=''>
-      <div>{year}</div>
-      <div>{mount}</div>
-      <div>{day}</div>
+    <div className='expense-date'>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__mount'>{mount}</div>
+      <div className='expense-date__day'>{day}</div>
     </div>
   )
 };
 
-export default ExpenseDate
+export default ExpenseDate;
