dcf1bf96e5a9f66bd683e58be04ff7376c2ec247
capocolo <gabrielserna.hce@gmail.com>
2022-10-10 16:01:36 +0200

    58.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -42,6 +42,7 @@ const ExpenseForm = () => {
       amount,
       date
     };
+
   };
 
   return (
@@ -49,15 +50,28 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input
+            type="text"
+            value={title}
+            onChange={titleChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" value={amount} onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={amount}
+            onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-01-01" max="2022-12-31" value={date} onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2022-12-31"
+            value={date}
+            onChange={dateChangeHandler}/>
         </div>
         <div className="new-expense__actions">
           <button type="submit" >Add expense</button>
