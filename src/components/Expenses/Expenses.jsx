1c90acd6e96e07bfda2225c8c38f5ace9e7b4585
capocolo <gabrielserna.hce@gmail.com>
2022-10-14 15:58:32 +0200

    2

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,19 +1,28 @@
 import Card from '../UI/Card.jsx';
 import ExpenseItem from './ExpenseItem.jsx';
+import ExpensesFilter from './ExpensesFilter.jsx';
 import './Expenses.css';
 
 const Expenses = (props) => {
+
+  const filterChangeHandler = (y) => {
+    console.log(`Expenses.js - ${y}`);
+  };
+
   return (
-    <Card className='expenses'>
-      { props.data.map( (el, idx) => (
+    <div>
+      <Card className='expenses'>
+      <ExpensesFilter onChangeFilterHandler={filterChangeHandler} />
+        { props.data.map( (el, idx) => (
           <ExpenseItem
-            key={idx}
-            title={el.title}
-            amount={el.amount}
-            date={el.date}
+          key={idx}
+          title={el.title}
+          amount={el.amount}
+          date={el.date}
           />
-      ))}
-    </Card>
+          ))}
+      </Card>
+    </div>
   );
 };
 
