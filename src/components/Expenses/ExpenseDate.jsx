4f64f71636d0cd5b7b13d559f13bf6cbb5b8cd78
capocolo <gabrielserna.hce@gmail.com>
2022-10-19 17:15:01 +0200

    merda

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -1,17 +1,19 @@
+import React from 'react';
+
 import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
-  const year = props.date.getFullYear();
-  const mount = props.date.toLocaleString('it-IT', { month: 'long' });
-  const day = props.date.toLocaleString('it-IT', { day: '2-digit' });
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  // const year = props.date.getFullYear();
 
   return (
     <div className='expense-date'>
-      <div className='expense-date__year'>{year}</div>
-      <div className='expense-date__mount'>{mount}</div>
+      <div className='expense-date__month'>{month}</div>
+      {/* <div className='expense-date__year'>{year}</div> */}
       <div className='expense-date__day'>{day}</div>
     </div>
-  )
+  );
 };
 
 export default ExpenseDate;
--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -5,22 +5,12 @@ import ExpenseDate from './ExpenseDate.jsx';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
-  const [amount, setAmount] = useState(props.amount);
-  const [date, setDate] = useState(props.date);
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Title Updated!')
-  };
-
   return (
     <Card className='expense-item'>
-      <ExpenseDate date={date} />
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
-        <button onClick={clickHandler}>Click Here</button>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </Card>
   )
